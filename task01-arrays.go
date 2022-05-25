bb3fe6f2b598e882c0e53cad6b6f4bb16a9fa030
stas-ik <stas-anikeyev@yandex.ru>
2022-05-25 21:42:35 +0300

    golang-united-school-homework-3- ready home work

--- task01-arrays.go
+++ task01-arrays.go
@@ -2,12 +2,12 @@ package homework
 
 func average(input [15]float32) (result float32) {
 
-	var summ float32 = 0
+	var summ float32
 
    	for i := 0; i < 15; i++ 
    	{
    		summ += input[i]
    	}
-   	
+
    	return summ/15
 }
